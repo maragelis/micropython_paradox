9842594e194c1a95d954db9018a94b0feea5d3da
Panagiotis <maragelis@gmail.com>
2022-07-16 10:21:01 +0300

    v1.10

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import threading
 from machine import UART ,WDT
 from time import sleep
+import utils
 
 print("hold 0 'boot button' for 5sec or ctrl-c to enter repl")
 
@@ -11,7 +12,7 @@ print("hold 0 'boot button' for 5sec or ctrl-c to enter repl")
 replloopcnt=0
 while replloopcnt < 10:
     if repl_button.value() == 0:
-        print("Dropping to REPL")
+        utils.trace("Dropping to REPL")
         sys.exit()
     else:
         replloopcnt +=1
@@ -20,9 +21,10 @@ while replloopcnt < 10:
         
     
             
-VERSION="1.01"
+VERSION="1.10"
 
 SEND_ALL_EVENTS = True
+
 
 #wdt = WDT(timeout=10000)
 
@@ -30,7 +32,7 @@ SEND_ALL_EVENTS = True
 client_id = ubinascii.hexlify(machine.unique_id())
 topic_sub = cfg.root_topicIn
 mqtt_server = cfg.mqttserver
-print(f"mqtt_server:{cfg.mqttserver}, username:{cfg.mqttusername}, password:{cfg.mqttpassword}, topic:{cfg.root_topicIn}")
+utils.trace(f"mqtt_server:{cfg.mqttserver}, username:{cfg.mqttusername}, password:{cfg.mqttpassword}, topic:{cfg.root_topicIn}")
 
 paradoxserial = UART(cfg.ESP_UART,baudrate=9600)
 paradoxserial.init()
@@ -46,6 +48,7 @@ PGMOFF = 0x33
 SET_DATE =0x30
 ARM_STATE=0x91
 PANEL_STATUS=0x50
+CLOSE_CONNECTION=0x70
 
 
 MESSAGE_LENGTH=37
@@ -125,8 +128,10 @@ class paradox_arm_status:
     sent=0
     
 
+    
+
 def sub_cb(topic, msg):
-    print((topic, msg))
+    utils.trace((topic, msg))
     if topic == bytes(cfg.root_topicIn,"utf-8") :
         strmsg = msg.decode("utf-8")
         json_data = json.loads(strmsg)
@@ -144,9 +149,9 @@ def sub_cb(topic, msg):
             else:
                 inmessage.subcommand = "0"
             
-            print(panel_control(inmessage))
+            utils.trace(panel_control(inmessage))
             
-        print(f"ESP received message : {inmessage}")
+        utils.trace(f"ESP received message : {inmessage}")
 
 def connect_and_subscribe():
   global client_id, mqtt_server, topic_sub
@@ -154,13 +159,13 @@ def connect_and_subscribe():
   client.set_callback(sub_cb)
   client.connect()
   client.subscribe(topic_sub)
-  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  utils.trace('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
   return client
 
 def restart_and_reconnect():
-  print('Failed to connect to MQTT broker. Reconnecting...')
+  utils.trace('Failed to connect to MQTT broker. Reconnecting...')
   time.sleep(10)
-  print('RESETTING ....')
+  utils.trace('RESETTING ....')
   machine.reset()    
 
 def serialRead():
@@ -168,7 +173,7 @@ def serialRead():
         
     if  paradoxserial.any() >= 37 :
         #reset_message()
-        print(f"serialRead has data {paradoxserial.any()}")
+        utils.trace(f"serialRead has data {paradoxserial.any()}")
         paradoxserial.readinto(MESSAGE)
         
         processMessage(MESSAGE)
@@ -189,13 +194,13 @@ def serialReadWriteQuick(byteMessage):
     loop = True
     while loop:
         if paradoxserial.any() >= 37 :
-            print(f"serialReadWriteQuick has data {paradoxserial.any()}")
+            utils.trace(f"serialReadWriteQuick has data {paradoxserial.any()}")
             paradoxserial.readinto(serial_message)
             if serial_message[0] & 0x10 == 0x10 :
-                print("Returning 0x10 message")
+                utils.trace("Returning 0x10 message")
                 return serial_message
             if serial_message[0] & 0x00 == 0x00 and serial_message[4] != 0x00 :
-                print(f"Returning 0x00 with serial_message[4] : {serial_message[4]}")
+                utils.trace(f"Returning 0x00 with serial_message[4] : {serial_message[4]}")
                 return serial_message
             loop=False
         
@@ -211,46 +216,46 @@ def serialWrite(byteMessage):
     
     
 def updateArmStatus(event, sub_event, partition):
-    print(f"updateArmStatus event:{event}, sub_event:{sub_event},partition:{partition} ")
+    utils.trace(f"updateArmStatus event:{event}, sub_event:{sub_event},partition:{partition} ")
     global hassioStatus
     datachanged = False
     hassioStatus[partition].Partition  = partition
     if (event == 2):
-        print("event:2")
+        utils.trace("event:2")
         if sub_event==4:
-            print("sub_event:4")
+            utils.trace("sub_event:4")
             
             hassioStatus[partition].stringArmStatus = "triggered"
             hassioStatus[partition].intArmStatus=4
             datachanged=True
-            print(f"ARM_STATE update : triggered")
+            utils.trace(f"ARM_STATE update : triggered")
         elif sub_event==11:
-            print("sub_event:11")
+            utils.trace("sub_event:11")
             hassioStatus[partition].stringArmStatus = "disarmed"
             hassioStatus[partition].intArmStatus = 3
             datachanged=True
-            print(f"ARM_STATE update : disarmed")
+            utils.trace(f"ARM_STATE update : disarmed")
         elif sub_event==12:
-            print("sub_event:12")
+            utils.trace("sub_event:12")
             hassioStatus[partition].stringArmStatus = "armed_away"
             hassioStatus[partition].intArmStatus = 1
             datachanged=True
-            print(f"ARM_STATE update : armed_away")
+            utils.trace(f"ARM_STATE update : armed_away")
     elif (event == 6):
-        print("event:6")
+        utils.trace("event:6")
         if (sub_event == 3):
-            print("sub_event:3")
+            utils.trace("sub_event:3")
             datachanged=True
             hassioStatus[partition].stringArmStatus = "armed_stay"
             hassioStatus[partition].intArmStatus = 0
-            print(f"ARM_STATE update : armed_home")
+            utils.trace(f"ARM_STATE update : armed_home")
         elif ( sub_event == 4):
-            print("sub_event:4")
+            utils.trace("sub_event:4")
             datachanged=True
             hassioStatus[partition].stringArmStatus = "armed_sleep"
             hassioStatus[partition].intArmStatus = 2
-            print(f"ARM_STATE update : armed_home")
-    print(f"hassioStatus:{hassioStatus[partition]}")     
+            utils.trace(f"ARM_STATE update : armed_home")
+    utils.trace(f"hassioStatus:{hassioStatus[partition]}")     
     
 def get_event_data(serial_message):
     e0msg = E0_message()
@@ -270,45 +275,45 @@ def get_event_data(serial_message):
         client.publish(cfg.root_topicOut, e0msg.toJson())
 
 def processMessage(serial_message):
-    #print("processMessage")
+    #utils.trace("processMessage")
     global hassioStatus,PANEL_IS_LOGGED_IN
-    print(f"processMessage serial_message is {hex(serial_message[0])} - {serial_message[0]}c ")
+    utils.trace(f"processMessage serial_message is {hex(serial_message[0])} - {serial_message[0]}c ")
     #for x in range(MESSAGE_LENGTH):
-    #    print(f"[serial_message{x}]={hex(serial_message[x])}")
+    #    utils.trace(f"[serial_message{x}]={hex(serial_message[x])}")
        
-    print(f"FIXED serial_message[0] {hex(serial_message[0] & 0xF0)} - {serial_message[0] & 0xF0}c ")    
+    utils.trace(f"FIXED serial_message[0] {hex(serial_message[0] & 0xF0)} - {serial_message[0] & 0xF0}c ")    
     if serial_message[0] & 0xF0 == 0xE0 :
-        print(f"Entired 0xE")
+        utils.trace(f"Entired 0xE")
         event=serial_message[7]
         sub_event=serial_message[8]
         partition=serial_message[9]
         
         if serial_message[7] == 0 or serial_message[7]==1:
-            print("E0 Zone Message receivied")
+            utils.trace("E0 Zone Message receivied")
             e = zone_message()
             e.zone=serial_message[8]
             e.state=True if serial_message[7] == 1 else False 
             e.Partition_Number=serial_message[9]
             e.zone_name=serial_message[15:30].decode().strip()
             e.topic = cfg.root_topicHassio + "/zone" + str(serial_message[8])
-            print(f"returning zoneJson  {e.toJson()}")
+            utils.trace(f"returning zoneJson  {e.toJson()}")
             client.publish(e.topic, e.toJson())
             
         
             
         elif (serial_message[7] == 48 and serial_message[8] == 3):
             PANEL_IS_LOGGED_IN = False
-            print(f"Panel Login from E0 message PANEL_IS_LOGGED_IN:{PANEL_IS_LOGGED_IN}")
+            utils.trace(f"Panel Login from E0 message PANEL_IS_LOGGED_IN:{PANEL_IS_LOGGED_IN}")
             
         elif (serial_message[7] == 48 and serial_message[8] == 2 ):
             PANEL_IS_LOGGED_IN = True
-            print(f"Panel Login from E0 message PANEL_IS_LOGGED_IN:{PANEL_IS_LOGGED_IN}")
+            utils.trace(f"Panel Login from E0 message PANEL_IS_LOGGED_IN:{PANEL_IS_LOGGED_IN}")
            
                    
         elif event == 2 or event == 6:
-            print(f"event:{event} updateArmStatus")
+            utils.trace(f"event:{event} updateArmStatus")
             updateArmStatus(event,sub_event, partition)
-            print(f"returned from updateArmStatus")
+            utils.trace(f"returned from updateArmStatus")
             
             if (event == 2 and sub_event == 11):
                 sendArmStatus(hassioStatus[partition])
@@ -317,18 +322,18 @@ def processMessage(serial_message):
                 sendArmStatus(hassioStatus[partition])
                 
         elif (event != 2 and sub_event != 12) :
-            print(f"process message event:{event},sub_event:{sub_event} sendArmStatus")
+            utils.trace(f"process message event:{event},sub_event:{sub_event} sendArmStatus")
             if (hassioStatus[partition].sent != hassioStatus[partition].intArmStatus):
                 hassioStatus[partition].sent = hassioStatus[partition].intArmStatus
                 sendArmStatus(hassioStatus[partition])  
         
         
-    print(f"Ended processMessage")
+    utils.trace(f"Ended processMessage")
       
             
 
 def program_init(version):
-    print(version)
+    utils.trace(version)
     return f"Program started ver:{version}".encode()
     
 
@@ -337,9 +342,10 @@ def panel_control(inCommand=inMessage()):
     
     global PANEL_IS_LOGGED_IN
     if not PANEL_IS_LOGGED_IN:
-        print(f"panel_control PANEL_IS_LOGGED_IN:{PANEL_IS_LOGGED_IN}")
+        utils.trace(f"panel_control PANEL_IS_LOGGED_IN:{PANEL_IS_LOGGED_IN}")
         panel_login(inCommand.panel_password)
-        print(f"panel_control PANEL_IS_LOGGED_IN:{PANEL_IS_LOGGED_IN}")
+        utils.trace(f"panel_control PANEL_IS_LOGGED_IN:{PANEL_IS_LOGGED_IN}")
+    
     
     panel_command = get_panel_command(inCommand.command)
     panel_subcommand = int(inCommand.subcommand) 
@@ -352,10 +358,10 @@ def panel_control(inCommand=inMessage()):
     armdata[35] = 0x00
     armdata = checksum_calculate(armdata)
     #for x in range(MESSAGE_LENGTH):
-    #    print(f"[armdata{x}]={hex(armdata[x])}")
+    #    utils.trace(f"[armdata{x}]={hex(armdata[x])}")
         
     if serialWrite(armdata):
-        print("armdata written to serial")
+        utils.trace("armdata written to serial")
         
     return True
 
@@ -365,13 +371,13 @@ def checksum_calculate(data):
     for x in range(MESSAGE_LENGTH-1):  
         checksum += data[x]
     
-    print(f"calculate checksum for {checksum}")
+    utils.trace(f"calculate checksum for {checksum}")
     while (checksum > 255) :
         intch =int(checksum / 256)
         checksum = checksum - intch * 256
-    print(f"checksum = {checksum}")
+    utils.trace(f"checksum = {checksum}")
     #checksum = checksum #& 0xFF
-    #print(type(checksum))
+    #utils.trace(type(checksum))
     data[36] =  checksum
     return data
 
@@ -390,14 +396,15 @@ def panel_login(panel_password):
     global COMUNTICATION_INIT,PANEL_LOGIN_IN_PROGRESS,PANEL_IS_LOGGED_IN
     
     PANEL_LOGIN_IN_PROGRESS=True
-    sleep(1)
+    #sleep(1)
     initMessage = initialize_comunitcation()
-    COMUNTICATION_INIT=True
     
-    print("Sent initialize_comunitcation")
+    
+    utils.trace("Sent initialize_comunitcation")
     rinitMessage = serialReadWriteQuick(initMessage)
-    print("returned initialize_comunitcation")
-    #print(rinitMessage)
+    COMUNTICATION_INIT=True
+    utils.trace("returned initialize_comunitcation")
+    #utils.trace(rinitMessage)
     
     pass1 = "0x" + str(panel_password)[0:2]
     pass2 = "0x" + str(panel_password)[2:4]
@@ -405,12 +412,12 @@ def panel_login(panel_password):
     data1=bytearray(MESSAGE_LENGTH)
     
     data1[0] = 0x00;
-    data1[4] = rinitMessage[4]& 0xff
-    data1[5] = rinitMessage[5]& 0xff
-    data1[6] = rinitMessage[6]& 0xff
-    data1[7] = rinitMessage[7]& 0xff
-    data1[7] = rinitMessage[8]& 0xff
-    data1[9] = rinitMessage[9]& 0xff
+    data1[4] = rinitMessage[4]
+    data1[5] = rinitMessage[5]
+    data1[6] = rinitMessage[6]
+    data1[7] = rinitMessage[7]
+    data1[7] = rinitMessage[8]
+    data1[9] = rinitMessage[9]
     data1[10] = 0x00;
     data1[11] = 0x00;
     data1[13] = 0x55;
@@ -432,11 +439,13 @@ def get_panel_command(arm_request):
     retval = 0
     if (arm_request == "stay" or arm_request=="0"):
         retval= STAY_ARM
+    elif (arm_request == "close" or arm_request=="1"):
+        retval= CLOSE_CONNECTION
     elif (arm_request == "arm" or arm_request=="1"):
         retval= FULL_ARM
     elif (arm_request == "sleep" or arm_request=="2"):
         retval= SLEEP_ARM
-    elif (arm_request == "disarm" or arm_request == "3"):
+    elif (arm_request == "disarm" or arm_request == "off" or arm_request == "3"):
         return DISARM
     elif (arm_request == "bypass" or arm_request == "10"):
         retval= BYPASS
@@ -456,7 +465,7 @@ def get_panel_command(arm_request):
 
   
 def sendArmStatus(hass):
-    print(f"SENDING ARM STATUS {hass}")
+    utils.trace(f"SENDING ARM STATUS {hass}")
     arm_mesg=arm_message()
     arm_mesg.topic = cfg.root_topicHassioArm + str(hass.Partition)
     arm_mesg.Armstatus =  hass.intArmStatus
@@ -475,7 +484,7 @@ def serialloop():
         try:
             client.check_msg()
             if PANEL_LOGIN_IN_PROGRESS != Serial_loop_msg:
-                  print(f"PANEL_LOGIN_IN_PROGRESS Status changed to {PANEL_LOGIN_IN_PROGRESS}")
+                  utils.trace(f"PANEL_LOGIN_IN_PROGRESS Status changed to {PANEL_LOGIN_IN_PROGRESS}")
                   Serial_loop_msg=PANEL_LOGIN_IN_PROGRESS
             if not PANEL_LOGIN_IN_PROGRESS:
               serialRead()            
@@ -506,7 +515,7 @@ if __name__ == '__main__':
     try:
         t1.start()
         #serialloop()
-        print("Starting Webserver")
+        utils.trace("Starting Webserver")
         websrv.runsrv()
     except:
         KILL_THREAD=True
---
+++ utils.py
@@ -0,0 +1,5 @@
+TRACE =False
+
+def trace(trace_message):
+    if TRACE:
+        print(trace_message);
